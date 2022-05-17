044a247be82a065233b7a4449f30b3d31e70bb9a
Aimal Amiri <meaimal@hotmail.com>
2022-05-18 02:16:00 +0430

    Add ingredients and source link to the modal

--- src/modules/Modal.js
+++ src/modules/Modal.js
@@ -5,13 +5,21 @@ export default class Modal {
   }
 
   load() {
-    console.log(this.data);
     const assignVlaue = (selector, value, attr) => {
       this.element.querySelector(`#modal-${selector}`)[attr ?? 'textContent'] = this.data[value];
+    };
+
+    for (let i = 1; i <= 20; i += 1) {
+      if (this.data[`strIngredient${i}`] !== '') {
+        const li = `<li class="badge">${this.data[`strIngredient${i}`]}</li>`
+        this.element.querySelector('#modal-ingredients').innerHTML += li;
+      }
     }
+
     assignVlaue('title', 'strMeal');
     assignVlaue('image', 'strMealThumb', 'src');
     assignVlaue('video', 'strYoutube', 'href');
+    assignVlaue('source', 'strSource', 'href');
     assignVlaue('instruction', 'strInstructions');
     assignVlaue('area', 'strArea');
     assignVlaue('category', 'strCategory');
