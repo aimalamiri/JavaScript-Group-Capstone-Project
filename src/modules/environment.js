dc17dfbce11d97e892f7a66da1ea36b6f4b512f1
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-05-18 12:52:37 -0500

    Fix linter error

--- src/modules/environment.js
+++ src/modules/environment.js
@@ -1,9 +1,7 @@
-const {
-  FREE_MEALS_URL,
-  INVOLVEMENT_URL,
-  FREE_MEALS_API_KEY,
-  INVOLVEMENT_API_KEY,
-} = process.env;
+const { FREE_MEALS_URL } = process.env;
+const { INVOLVEMENT_URL } = process.env;
+const { FREE_MEALS_API_KEY } = process.env;
+const { INVOLVEMENT_API_KEY } = process.env;
 
 export {
   FREE_MEALS_URL,
