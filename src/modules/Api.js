8afa7871bd0d533dd9dae31fb51ed69a1cf1a416
Aimal Amiri <meaimal@hotmail.com>
2022-05-18 21:40:20 +0430

    Add fetching the comments from server

--- src/modules/Api.js
+++ src/modules/Api.js
@@ -7,63 +7,84 @@ class Api {
     this.involvementUrl = involvementUrl;
   }
 
-  getCategories = () => fetch(`${this.baseUrl}/categories.php`)
-    .then((response) => response.json())
-    .then((json) => json.categories)
-    .catch((error) => {
-      throw error;
-    });
+  getCategories = () =>
+    fetch(`${this.baseUrl}/categories.php`)
+      .then((response) => response.json())
+      .then((json) => json.categories)
+      .catch((error) => {
+        throw error;
+      });
 
-  getByCategory = (category) => fetch(`${this.baseUrl}/filter.php?c=${category}`)
-    .then((response) => response.json())
-    .then((json) => json.meals)
-    .catch((error) => {
-      throw error;
-    });
+  getByCategory = (category) =>
+    fetch(`${this.baseUrl}/filter.php?c=${category}`)
+      .then((response) => response.json())
+      .then((json) => json.meals)
+      .catch((error) => {
+        throw error;
+      });
 
-  getMeal = (idMeal) => fetch(`${this.baseUrl}/lookup.php?i=${idMeal}`)
-    .then((response) => response.json())
-    .then((json) => json.meals)
-    .then((list) => (list.length ? list[0] : {}))
-    .catch((error) => {
-      throw error;
-    });
+  getMeal = (idMeal) =>
+    fetch(`${this.baseUrl}/lookup.php?i=${idMeal}`)
+      .then((response) => response.json())
+      .then((json) => json.meals)
+      .then((list) => (list.length ? list[0] : {}))
+      .catch((error) => {
+        throw error;
+      });
 
-  createApp = async () => fetch(`${this.involvementUrl}/apps/`, {
-    method: 'POST',
-    body: '',
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  })
-    .then((response) => response.text())
-    .catch((error) => {
-      throw error;
-    });
-
-  addLike = async (idApp, idMeal) => fetch(`${this.involvementUrl}/apps/${idApp}/likes/`, {
-    method: 'POST',
-    body: JSON.stringify({ item_id: idMeal }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  })
-    .then(((response) => response.text()))
-    .catch((error) => {
-      throw error;
-    });
-
-  getLikes = (idApp) => fetch(`${this.involvementUrl}/apps/${idApp}/likes/`)
-    .then((response) => {
-      const responseLength = Number(response.headers.get('content-length'));
-      if (responseLength > 0) {
-        return response.json();
-      }
-      return [];
+  createApp = async () =>
+    fetch(`${this.involvementUrl}/apps/`, {
+      method: 'POST',
+      body: '',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
     })
-    .catch((error) => {
-      throw error;
-    });
+      .then((response) => response.text())
+      .catch((error) => {
+        throw error;
+      });
+
+  addLike = async (idApp, idMeal) =>
+    fetch(`${this.involvementUrl}/apps/${idApp}/likes/`, {
+      method: 'POST',
+      body: JSON.stringify({ item_id: idMeal }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.text())
+      .catch((error) => {
+        throw error;
+      });
+
+  getLikes = (idApp) =>
+    fetch(`${this.involvementUrl}/apps/${idApp}/likes/`)
+      .then((response) => {
+        const responseLength = Number(response.headers.get('content-length'));
+        if (responseLength > 0) {
+          return response.json();
+        }
+        return [];
+      })
+      .catch((error) => {
+        throw error;
+      });
+
+  getComments = async (idApp, mealId) => {
+    let result;
+    await fetch(`${this.involvementUrl}/apps/${idApp}/comments?item_id=${mealId}`)
+      .then((response) => {
+        result = response.json();
+        if (!response.ok) {
+          result = [];
+        }
+      })
+      .catch((error) => {
+        throw new Error('No comments found for this meal!');
+      });
+    return result;
+  };
 }
 
 const api = new Api();
