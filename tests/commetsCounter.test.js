d62b4aed55e684944de31950df8a22962c40b8f3
Aimal Amiri <meaimal@hotmail.com>
2022-05-19 19:51:30 +0430

    Refactor the comments test code

--- tests/commetsCounter.test.js
+++ tests/commetsCounter.test.js
@@ -6,21 +6,20 @@ const comments = [
 ];
 
 describe('Comments test', () => {
-  it('Should insert and count the comments', () => {
+  beforeEach(() => {
     document.body.innerHTML = `
       <h3 class="mt-3 text-lg"><span id="modal-comments-count" class="font-bold"></span> comments</h3>
       <ul id="modal-comments"></ul>
     `;
+  });
+
+  it('Should insert and count the comments', () => {
     loadComments(comments);
     const commentsCount = document.querySelector('#modal-comments-count').textContent;
     expect(comments.length).toEqual(Number(commentsCount));
   });
 
   it('Should have "No" comments when an empty array is passed in.', () => {
-    document.body.innerHTML = `
-      <h3 class="mt-3 text-lg"><span id="modal-comments-count" class="font-bold"></span> comments</h3>
-      <ul id="modal-comments"></ul>
-    `;
     loadComments([]);
     const commentsCount = document.querySelector('#modal-comments-count').textContent;
     expect('No').toBe(commentsCount);
